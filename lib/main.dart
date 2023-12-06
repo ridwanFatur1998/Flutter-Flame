8fc94fc89c611d20b5d1b0cf5d99391a0c83aeac
LAPTOP-7LOBBHA8\Ridwan Faturrahman <ridwan.faturrahman.godjali@gmail.com>
2023-12-06 09:45:16 +0700

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,89 +1,27 @@
-import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
-import 'package:flame/components.dart';
-import 'dart:math' as math;
+import 'dino_game.dart';
+import 'helpers/navigation_keys.dart';
 
 void main() {
+  final game = DinoGame();
+  
   runApp(
-    GameWidget(
-      game: MyGame(),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            GameWidget(
+              game: game,
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: NavigationKeys(onDirectionChanged: game.onArrowKeyChanged,),
+            ),
+          ],
+        ),
+      ),
     ),
   );
-}
-
-class MyGame extends FlameGame with TapCallbacks {
-  @override
-  Future<void> onLoad() async {
-    add(Square(size / 2));
-  }
-
-  @override
-  void onTapDown(TapDownEvent event) {
-    super.onTapDown(event);
-    if (!event.handled) {
-      final touchPoint = event.canvasPosition;
-      add(Square(touchPoint));
-    }
-  }
-}
-
-class Square extends RectangleComponent with TapCallbacks {
-  static const speed = 3;
-  static const squareSize = 128.0;
-  static const indicatorSize = 12.0;
-
-  static final Paint red = BasicPalette.red.paint();
-  static final Paint blue = BasicPalette.blue.paint();
-  static final Paint yellow = BasicPalette.yellow.paint();
-  static final Paint green = BasicPalette.green.paint();
-
-  Square(Vector2 position)
-      : super(
-          position: position,
-          size: Vector2.all(squareSize),
-          anchor: Anchor.center,
-          paint: yellow,
-        );
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    angle += speed * dt;
-    angle %= 2 * math.pi;
-  }
-
-  @override
-  Future<void> onLoad() async {
-    super.onLoad();
-    add(
-      RectangleComponent(
-        size: Vector2.all(indicatorSize),
-        paint: blue,
-      ),
-    );
-    add(
-      RectangleComponent(
-        size: Vector2.all(indicatorSize),
-        paint: green,
-        position: size / 2,
-        anchor: Anchor.center,
-      ),
-    );
-    add(
-      RectangleComponent(
-        position: size / 2,
-        size: Vector2.all(indicatorSize),
-        anchor: Anchor.center,
-        paint: red,
-      ),
-    );
-  }
-
-  @override
-  void onTapDown(TapDownEvent event) {
-    removeFromParent();
-    event.handled = true;
-  }
 }
